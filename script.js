fd84da89cb4ca326d0b320b2a792a16e8ecbc834
emam04bd <emam04bd@gmail.com>
2025-07-20 13:36:48 +0600

    Plan project

---
+++ script.js
@@ -0,0 +1,19 @@
+// function getPlayerChoice()
+//   return player input
+
+// function getBotChoice()
+//   return random choice
+
+// function playRound()
+//   take user input 
+//   show computer choice
+//   show result
+//   return result
+
+// playGame()
+//   play 5 rounds
+//   keep scores
+//     variables that counts number of games, computers score, players score, number of ties
+//   show stats
+//   update states
+//   Declare winner and loser
